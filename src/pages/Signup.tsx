d3e80663a4d5c7a5e10cad69b0577ce182ed6937
Abhishek Sharma <abhisharma4950@gmail.com>
2024-10-10 14:10:45 -0400

    test

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,88 +1,88 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  emailState,
-  passwordState,
-  quoteState,
-  usernameState,
-} from "@/recoil/atom";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useRecoilState } from "recoil";
+// import {
+//   emailState,
+//   passwordState,
+//   quoteState,
+//   usernameState,
+// } from "@/recoil/atom";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useRecoilState } from "recoil";
 
-interface Quote {
-  quote: string;
-  author: string;
-}
+// interface Quote {
+//   quote: string;
+//   author: string;
+// }
 
 export const Signup = () => {
-  const navigate = useNavigate();
-  const [quote, setQuote] = useRecoilState<Quote | null>(quoteState);
-  const [email, setEmail] = useRecoilState(emailState);
-  const [username, setUsername] = useRecoilState(usernameState);
-  const [password, setPassword] = useRecoilState(passwordState);
-  const [file, setFile] = useState<File | null>(null);
+  // const navigate = useNavigate();
+  // const [quote, setQuote] = useRecoilState<Quote | null>(quoteState);
+  // const [email, setEmail] = useRecoilState(emailState);
+  // const [username, setUsername] = useRecoilState(usernameState);
+  // const [password, setPassword] = useRecoilState(passwordState);
+  // const [file, setFile] = useState<File | null>(null);
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setFile(e.target.files[0]); // Set the first file if available
-    } else {
-      setFile(null); // Clear the file state if no file is selected
-    }
-  };
+  // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files && e.target.files.length > 0) {
+  //     setFile(e.target.files[0]); // Set the first file if available
+  //   } else {
+  //     setFile(null); // Clear the file state if no file is selected
+  //   }
+  // };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.api-ninjas.com/v1/quotes?category=happiness",
-          {
-            headers: {
-              "X-Api-Key": "Uc899KMmjxTGJ2vmiv7Tdg==1u9iHsIcFgGV4qlj",
-            },
-          }
-        );
-        console.log(response.data[0]);
-        setQuote(response.data[0]);
-      } catch (error) {
-        console.error("Error fetching the quote:", error);
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "https://api.api-ninjas.com/v1/quotes?category=happiness",
+  //         {
+  //           headers: {
+  //             "X-Api-Key": "Uc899KMmjxTGJ2vmiv7Tdg==1u9iHsIcFgGV4qlj",
+  //           },
+  //         }
+  //       );
+  //       console.log(response.data[0]);
+  //       setQuote(response.data[0]);
+  //     } catch (error) {
+  //       console.error("Error fetching the quote:", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   const handleClick = async () => {
-    const formData = new FormData();
+    // const formData = new FormData();
 
-    formData.append("email", email);
-    formData.append("name", username);
-    formData.append("password", password);
-    if (file) {
-      formData.append("avatar", file);
-    }
-    try {
-      const response = await axios.post(
-        "https://backend.abhisharma4950.workers.dev/user/signup",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+    // formData.append("email", email);
+    // formData.append("name", username);
+    // formData.append("password", password);
+    // if (file) {
+    //   formData.append("avatar", file);
+    // }
+    // try {
+    //   const response = await axios.post(
+    //     "https://backend.abhisharma4950.workers.dev/user/signup",
+    //     formData,
+    //     {
+    //       headers: {
+    //         "Content-Type": "multipart/form-data",
+    //       },
+    //     }
+    //   );
 
-      if (response.status === 200) {
-        console.log("User added to db");
-        localStorage.setItem("token", response.data.token);
-        navigate("/signin");
-      } else {
-        console.log("Error while signing up user");
-      }
-    } catch (error) {
-      console.error("Error during signup:", error);
-    }
+    //   if (response.status === 200) {
+    //     console.log("User added to db");
+    //     localStorage.setItem("token", response.data.token);
+    //     navigate("/signin");
+    //   } else {
+    //     console.log("Error while signing up user");
+    //   }
+    // } catch (error) {
+    //   console.error("Error during signup:", error);
+    // }
   };
 
   return (
@@ -107,7 +107,7 @@ export const Signup = () => {
               id="username"
               type="text"
               placeholder="Enter your username"
-              onChange={(e) => setUsername(e.target.value)}
+              // onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -117,7 +117,7 @@ export const Signup = () => {
               id="email"
               type="email"
               placeholder="Enter your email"
-              onChange={(e) => setEmail(e.target.value)}
+              // onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -127,13 +127,13 @@ export const Signup = () => {
               id="password"
               type="password"
               placeholder="Enter your password"
-              onChange={(e) => setPassword(e.target.value)}
+              // onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
           <div className="xs:w-[200px] tab:w-[250px] sm:w-[300px] xl:w-[350px] mt-5 grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="picture">Profile Picture</Label>
-            <Input id="picture" type="file" onChange={handleFileChange} />
+            {/* <Input id="picture" type="file" onChange={handleFileChange} /> */}
           </div>
 
           <div className="mt-5 ">
@@ -149,7 +149,7 @@ export const Signup = () => {
       </div>
 
       {/* Right Side */}
-      <div className="w-1/2 bg-rose-100 flex justify-center content-center flex-col hidden md:block">
+      {/* <div className="w-1/2 bg-rose-100 flex justify-center content-center flex-col hidden md:block">
         <div className="flex items-center flex-col">
           {quote && (
             <div className="md:text-xl xl:text-2xl font-bold tracking-tight m-4 text-center">
@@ -160,7 +160,7 @@ export const Signup = () => {
             </div>
           )}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
